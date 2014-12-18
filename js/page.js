f90c9093cdc9471ffd9ff4dbb45730a042b748b0
Katie <kreiner@umich.edu>
2014-12-17 19:15:58 -0500

    finished reps pages

--- js/page.js
+++ js/page.js
@@ -1,9 +1,86 @@
 $( document ).ready(function() {
-	$("#hide").click(function(){
-	  $("p").hide();
+	 $("#stevie").hide();
+
+	$("#upenn").click(function(){
+	  $("#stevie").toggle();
 	});
 
-	$("#show").click(function(){
-	  $("p").show();
+
+	 $("#ali").hide();
+
+	$("#vandy").click(function(){
+	  $("#ali").toggle();
 	});
+	 $(".wiscorep").hide();
+
+	$("#wisco").click(function(){
+	  $(".wiscorep").toggle();
+	});
+
+	 $("#amanda").hide();
+
+	$("#delaware").click(function(){
+	  $("#amanda").toggle();
+	});
+	 $(".floridareps").hide();
+
+	$("#florida").click(function(){
+	  $(".floridareps").toggle();
+	});
+	 $("#emma").hide();
+
+	$("#lehigh").click(function(){
+	  $("#emma").toggle();
+	});
+	 $(".psureps").hide();
+
+	$("#psu").click(function(){
+	  $(".psureps").toggle();
+	});
+	 $(".boulderreps").hide();
+
+	$("#boulder").click(function(){
+	  $(".boulderreps").toggle();
+	});
+	 $("#kim").hide();
+
+	$("#bu").click(function(){
+	  $("#kim").toggle();
+	});
+	 $("#morgan").hide();
+
+	$("#emory").click(function(){
+	  $("#morgan").toggle();
+	});
+	 $(".syracusereps").hide();
+
+	$("#syracuse").click(function(){
+	  $(".syracusereps").toggle();
+	});
+	 $("#sydney").hide();
+
+	$("#tulane").click(function(){
+	  $("#sydney").toggle();
+	});
+	 $(".indianareps").hide();
+
+	$("#indiana").click(function(){
+	  $(".indianareps").toggle();
+	});
+	 $("#dani").hide();
+
+	$("#nw").click(function(){
+	  $("#dani").toggle();
+	});
+	 $("#jessie").hide();
+
+	$("#maryland").click(function(){
+	  $("#jessie").toggle();
+	});
+	 $("#hayley").hide();
+
+	$("#miami").click(function(){
+	  $("#hayley").toggle();
+	});
+	 
 });
